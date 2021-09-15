78c3c02aefaa9aa3c62a7d8f7ac20437bb1c30e8
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-09-14 20:40:06 -0400

    Added create row and create grid methods to class Game in game.py file.

--- game.py
+++ game.py
@@ -5,14 +5,14 @@ import random
 class Game:
     def __init__(self):
         self.size = 4
-        self.card_options = ['Add', 'Boo', 'Cat', 'Dev', 'Egg', 'Far', 'Gum', 'Hut']
-        self.columns = ['A', 'B', 'C', 'D']
+        self.card_options = ('Add', 'Boo', 'Cat', 'Dev', 'Egg', 'Far', 'Gum', 'Hut')
+        self.columns = ('A', 'B', 'C', 'D')
         self.cards = []
         self.locations = []
         for column in self.columns:
             for num in range(1, self.size + 1):
-                # print(f'{column}{num}')
                 self.locations.append(f'{column}{num}')
+                # print(f'{column}{num}')
 
     def set_cards(self):
         used_locations = []
@@ -24,9 +24,36 @@ class Game:
                 card = Card(word, random_location)
                 self.cards.append(card)
 
+    def create_row(self, num):
+        row = []
+        for column in self.columns:
+            for card in self.cards:
+                if card.location == f'{column}{num}':
+                    if card.matched:
+                        row.append(str(card))
+                    else:
+                        row.append("   ")
+        return row
+
+    def create_grid(self):
+        header = '  |  ' + '  |  '.join(self.columns) + '  |'
+        print(header)
+        for row in range(1, self.size + 1):
+            print_row = f'{row} | '
+            get_row = self.create_row(row)
+            print_row += ' | '.join(get_row) + ' |'
+            print(print_row)
+
 
 if __name__ == "__main__":
     game = Game()
     game.set_cards()
-    for card1 in game.cards:
-        print(card1)
+    game.cards[0].matched = True
+    game.cards[1].matched = True
+    game.cards[2].matched = True
+    game.cards[3].matched = True
+    game.create_grid()
+    # print(game.create_row(1))
+    # print(game.create_row(2))
+    # print(game.create_row(3))
+    # print(game.create_row(4))
