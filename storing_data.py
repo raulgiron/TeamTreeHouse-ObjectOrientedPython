606be9816680979426de996a53dcd4bb9550adee
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-09-21 05:55:35 -0400

    Refactorized the greet user program.

--- storing_data.py
+++ storing_data.py
@@ -1,31 +1,31 @@
 import json
 import requests
 
-# numbers = [2, 3, 5, 7, 11, 13]
-#
-# filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/numbers.json'
-# with open(filename, 'w') as file_object:
-#     json.dump(numbers, file_object)
-#
-# with open(filename, 'r') as file_object:
-#     numbers = json.load(file_object)
-#
-# print(numbers)
-#
-#
-# filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/username.json'
-#
-# try:
-#     with open(filename, 'r') as file_object:
-#         username = json.load(file_object)
-# except FileNotFoundError:
-#     username = input(f"What is your name? ")
-#     with open(filename, 'w') as file_object:
-#         json.dump(username.title(), file_object)
-#         print(f"we'll remember you when you came back, {username.title()}!")
-# else:
-#     print(f"Welcome back, {username}!")
+numbers = [2, 3, 5, 7, 11, 13]
 
+filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/numbers.json'
+with open(filename, 'w') as file_object:
+    json.dump(numbers, file_object)
+
+with open(filename, 'r') as file_object:
+    numbers = json.load(file_object)
+
+print(numbers)
+
+
+filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/username.json'
+
+try:
+    with open(filename, 'r') as file_object:
+        username = json.load(file_object)
+except FileNotFoundError:
+    username = input(f"What is your name? ")
+    with open(filename, 'w') as file_object:
+        json.dump(username.title(), file_object)
+        print(f"we'll remember you when you came back, {username.title()}!")
+else:
+    print(f"Welcome back, {username}!")
+#
 filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/incremental.json'
 
 try:
@@ -43,15 +43,49 @@ else:
         print(f"The counter number is: {counting} ")
 
 # Making a get request
-# filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/api_data.json'
-# response = requests.get("https://api.github.com")
-# data = response.json()
-# print(type(data))  # <class 'dict'>
-# print(data)
+filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/api_data.json'
+response = requests.get("https://api.github.com")
+data = response.json()
+print(type(data))  # <class 'dict'>
+print(data)
 
-# with open(filename, 'w') as file_object:
-#     json.dump(data, file_object)
+with open(filename, 'w') as file_object:
+    json.dump(data, file_object)
 
-# with open(filename, 'r') as file_object:
-#     git_hub = json.load(file_object)
-#     print(git_hub)
+with open(filename, 'r') as file_object:
+    git_hub = json.load(file_object)
+    print(git_hub)
+
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/username.json'
+    try:
+        with open(filename, 'r') as file_object:
+            username = json.load(file_object)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/username.json'
+    with open(filename, 'w') as file_object:
+        json.dump(username, file_object)
+    return username
+
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+
+greet_user()
--- working_with_files.py
+++ working_with_files.py
@@ -1,82 +1,82 @@
 import json
 
-# filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/programming.txt'
-#
-# with open(filename, 'w') as file_object:
-#     file_object.write(f"I love programming.\n")
-#     file_object.write(f"\tI love creating new games.\n")
-#     file_object.write(f"\t\tI also love finding meaning in large datasets.\n")
-#     file_object.write(f"\t\t\tI love creating apps that can run in a browser.\n")
-#
-# with open(filename, 'r') as file_object:
-#     lines = file_object.readlines()
-#     print(lines)
-#     file = ''
-#     for line in lines:
-#         file += line
-# print(file)
+filename = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/programming.txt'
 
-# filename = 'guest.txt'
-#
-# with open(filename, 'w') as file_object:
-#     name = input("What is your full name?")
-#     file_object.write(name)
-#
-# with open(filename, 'r') as file_object:
-#     name = str(file_object.readline().title())
-#     print(name)
+with open(filename, 'w') as file_object:
+    file_object.write(f"I love programming.\n")
+    file_object.write(f"\tI love creating new games.\n")
+    file_object.write(f"\t\tI also love finding meaning in large datasets.\n")
+    file_object.write(f"\t\t\tI love creating apps that can run in a browser.\n")
 
-# filename = 'guest.txt'
-# index = 0
-# while True:
-#     print(f"Press letter 'Q' to exit the program.")
-#     name = input("What is your full name? ")
-#     if name.lower() == 'q':
-#         break
-#     index += 1
-#     print(f"Hi, {name.title()}, welcome to the system!")
-#     with open(filename, 'a') as file_object:
-#         file_object.write(f'{index}.- {name.title()}\n')
+with open(filename, 'r') as file_object:
+    lines = file_object.readlines()
+    print(lines)
+    file = ''
+    for line in lines:
+        file += line
+print(file)
+
+filename = 'guest.txt'
+
+with open(filename, 'w') as file_object:
+    name = input("What is your full name?")
+    file_object.write(name)
+
+with open(filename, 'r') as file_object:
+    name = str(file_object.readline().title())
+    print(name)
+
+filename = 'guest.txt'
+index = 0
+while True:
+    print(f"Press letter 'Q' to exit the program.")
+    name = input("What is your full name? ")
+    if name.lower() == 'q':
+        break
+    index += 1
+    print(f"Hi, {name.title()}, welcome to the system!")
+    with open(filename, 'a') as file_object:
+        file_object.write(f'{index}.- {name.title()}\n')
 
 filename1 = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/programming.txt'
-# filename2 = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/incremental.json'
+filename2 = '/Users/raulgiron/Desktop/TeamTreeHouse/TeamTreeHouse-ObjectOrientedPython/incremental.json'
 
-# while True:
-#     print(f"Press letter 'Q' to exit the program.")
-#     name = input("What's your name? ")
-#     if name.lower() == 'q':
-#         break
-#     answer = input("Why do you like computer programming? ")
-#     if answer.lower() == 'q':
-#         break
-#     try:
-#         with open(filename2, 'r') as file_object:
-#             counting = json.load(file_object)
-#     except FileNotFoundError:
-#         counting = 1
-#         with open(filename2, 'w') as file_object:
-#             json.dump(counting, file_object)
-#             print(f"A file was created with the following number: {counting} ")
-#     else:
-#         counting += 1
-#         with open(filename2, 'w') as file_object:
-#             json.dump(counting, file_object)
-#             print(f"The counter number is: {counting} ")
-#     try:
-#         with open(filename1, 'r') as file_object:
-#             lines = file_object.readlines()
-#             file = ''
-#             for line in lines:
-#                 file += line
-#             print(file)
-#     except FileNotFoundError:
-#         with open(filename1, 'w') as file_object:
-#             file_object.write(f"{counting}-) Hi {name.title()}! Why do you like computer programming?\n")
-#             file_object.write(f"    {counting}.- {answer}\n\n")
-#     else:
-#         with open(filename1, 'a') as file_object:
-#             file_object.write(f"{counting}-) Hi {name.title()}! Why do you like computer programming?\n")
-#             file_object.write(f"    {counting}.- {answer}\n\n")
+while True:
+    print(f"Press letter 'Q' to exit the program.")
+    name = input("What's your name? ")
+    if name.lower() == 'q':
+        break
+    answer = input("Why do you like computer programming? ")
+    if answer.lower() == 'q':
+        break
+    try:
+        with open(filename2, 'r') as file_object:
+            counting = json.load(file_object)
+    except FileNotFoundError:
+        counting = 1
+        with open(filename2, 'w') as file_object:
+            json.dump(counting, file_object)
+            print(f"A file was created with the following number: {counting} ")
+    else:
+        counting += 1
+        with open(filename2, 'w') as file_object:
+            json.dump(counting, file_object)
+            print(f"The counter number is: {counting} ")
+    try:
+        with open(filename1, 'r') as file_object:
+            lines = file_object.readlines()
+            file = ''
+            for line in lines:
+                file += line
+            print(file)
+    except FileNotFoundError:
+        with open(filename1, 'w') as file_object:
+            file_object.write(f"{counting}-) Hi {name.title()}! Why do you like computer programming?\n")
+            file_object.write(f"    {counting}.- {answer}\n\n")
+    else:
+        with open(filename1, 'a') as file_object:
+            file_object.write(f"{counting}-) Hi {name.title()}! Why do you like computer programming?\n")
+            file_object.write(f"    {counting}.- {answer}\n\n")
 
 with open(filename1, 'r') as file_object:
     encuestados = file_object.readlines()
