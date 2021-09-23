2e8cc2861f77e64598cea3a48260a24a921fb87f
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-09-23 02:45:29 -0400

    Handling Class errors with unit test module in python.

--- handling_error.py
+++ handling_error.py
@@ -1,27 +1,27 @@
-from termcolor import colored
-
-while True:
-    try:
-        print("TYPE -999- TO EXIT THE PROGRAM!")
-        dividend: int = int(input(colored("Please type -dividend- number: ", 'blue')))
-        if int(dividend) == 999:
-            break
-    except ValueError:
-        print(colored(f"WARNING: - Dividend must be a number.\n \t * Please type a number no a letter.", 'red'))
-        continue
-    try:
-        divisor: int = int(input(colored("Please type -divisor- number: ", 'magenta')))
-        if int(divisor) == 999:
-            break
-    except ValueError:
-        print(colored(f"WARNING: - Divisor must be a number.\n \t * Please type a number no a letter.", 'red'))
-        continue
-    try:
-        quotient: float = float(dividend / divisor)
-    except ZeroDivisionError:
-        print(f"\tYou can't divide by zero!")
-    else:
-        print(f"{dividend} / {divisor} = {quotient}")
+# from termcolor import colored
+#
+# while True:
+#     try:
+#         print("TYPE -999- TO EXIT THE PROGRAM!")
+#         dividend: int = int(input(colored("Please type -dividend- number: ", 'blue')))
+#         if int(dividend) == 999:
+#             break
+#     except ValueError:
+#         print(colored(f"WARNING: - Dividend must be a number.\n \t * Please type a number no a letter.", 'red'))
+#         continue
+#     try:
+#         divisor: int = int(input(colored("Please type -divisor- number: ", 'magenta')))
+#         if int(divisor) == 999:
+#             break
+#     except ValueError:
+#         print(colored(f"WARNING: - Divisor must be a number.\n \t * Please type a number no a letter.", 'red'))
+#         continue
+#     try:
+#         quotient: float = float(dividend / divisor)
+#     except ZeroDivisionError:
+#         print(f"\tYou can't divide by zero!")
+#     else:
+#         print(f"{dividend} / {divisor} = {quotient}")
 
 
 def get_formatted_name(first, last, middle=''):
--- unit_test.py
+++ unit_test.py
@@ -16,5 +16,50 @@ class NameTestCase(unittest.TestCase):
         self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 
+class AnonymousSurvey:
+    """Collect anonymous answers to a survey question."""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+
+    def show_question(self,):
+        """Show the survey question."""
+        print(self.question)
+
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all the responses that have benn given."""
+        print("Survey results:")
+        for response in self.responses:
+            print(f"- {response}")
+
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Test for the class AnonymousSurvey."""
+
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods."""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['Spanish', 'English', 'Mandarin']
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+
 if __name__ == '__main__':
     unittest.main()
