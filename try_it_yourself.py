9d6fa35af26b31d7e0f58098853475038bc9a6de
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-09-24 03:47:28 -0400

    Created Lottery class.

--- try_it_yourself.py
+++ try_it_yourself.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from random import sample
 
 class Die:
     """Roll Die class. This is a basic learning project."""
@@ -13,6 +13,21 @@ class Die:
         return die
 
 
+class Lottery:
+    """Winner of lottery. This is a basic learning project."""
+
+    def __init__(self):
+        self.lottery_database = (14, 5, 'R', 36, 56, 'D', 63, 96, 'A', 68, 85, 'J', 14, 45, 'V')
+
+    def lottery_winner(self):
+        winner = sample(self.lottery_database, 4)
+        print(f"Congratulations, any ticket matching these four numbers or letters wint a prize: \n\t{winner}")
+        return winner
+
+
 if __name__ == "__main__":
     dado = Die()
     dado.roll_die()
+    concurso = Lottery()
+    concurso.lottery_winner()
+    print(type(concurso.lottery_winner()))
